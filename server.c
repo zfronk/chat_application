2fb4a422a18aaaf5e0253d5ab8b3508c21bf598e
zfronk <officialstevve@gmail.com>
2025-03-25 16:48:11 +0300

    second commit

--- server.c
+++ server.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
+#include <arpa/inet.h>
 
-// BASIC HTTP SERVER IN C
+
+// Basic Http Server
+
+// Steps
+// 1. Create socket for communication
+// 2. Bind the socket to a port
+// 3. Listen for incoming connections
+// 4. Accept the connection
+// 5. Read the request
+// 6. Send the response
+// 7. Close the connection
+
+
 
 // Main entry point
 int main(){
+
 
     return 0;
 }
