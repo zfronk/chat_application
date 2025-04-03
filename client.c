7c83ab076a591f2c588698dfd313d889f0f41523
zfronk <officialstevve@gmail.com>
2025-04-03 03:43:11 +0300

    ai based

---
+++ client.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    char message[BUFFER_SIZE];
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    while (1) {
+        printf("Enter message: ");
+        fgets(message, BUFFER_SIZE, stdin);
+        send(sock, message, strlen(message), 0);
+        valread = read(sock, buffer, BUFFER_SIZE);
+        printf("Server response: %s", buffer);
+        memset(buffer, 0, BUFFER_SIZE); // clear the buffer.
+    }
+
+    close(sock);
+    return 0;
+}
--- server.c
+++ server.c
@@ -4,22 +4,81 @@
 #include <arpa/inet.h>
 
 
-// Basic Http Server
-
-// Steps
-// 1. Create socket for communication
-// 2. Bind the socket to a port
-// 3. Listen for incoming connections
-// 4. Accept the connection
-// 5. Read the request
-// 6. Send the response
-// 7. Close the connection
-
+/* Steps
+1. Create socket for communication
+2. Bind the socket to a port
+3. Listen for incoming connections
+4. Accept the connection
+5. Read the request
+6. Send the response
+7. Close the connection
+*/
 
 
 // Main entry point
 int main(){
 
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <unistd.h>
+	#include <arpa/inet.h>
+	#include <sys/socket.h>
+	
+	#define PORT 8080
+	#define BUFFER_SIZE 1024
 
-    return 0;
+int main(){
+		int server_fd, new_socket, valread;
+	    struct sockaddr_in address;
+	    int opt = 1;
+	    int addrlen = sizeof(address);
+	    char buffer[BUFFER_SIZE] = {0};
+	
+	    // Create socket
+	    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+	        perror("socket failed");
+	        exit(EXIT_FAILURE);
+	    }
+	
+	    // Set socket options
+	    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+	        perror("setsockopt");
+	        exit(EXIT_FAILURE);
+	    }
+	
+	    address.sin_family = AF_INET;
+	    address.sin_addr.s_addr = INADDR_ANY;
+	    address.sin_port = htons(PORT);
+	
+	    // Bind socket to address
+	    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+	        perror("bind failed");
+	        exit(EXIT_FAILURE);
+	    }
+	
+	    // Listen for connections
+	    if (listen(server_fd, 3) < 0) {
+	        perror("listen");
+	        exit(EXIT_FAILURE);
+	    }
+	
+	    // Accept connection
+	    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+	        perror("accept");
+	        exit(EXIT_FAILURE);
+	    }
+	
+	    // Receive and send data
+	    while ((valread = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
+	        printf("Received: %s", buffer);
+	        send(new_socket, buffer, strlen(buffer), 0);
+	        memset(buffer, 0, BUFFER_SIZE); // clear the buffer.
+	    }
+	
+	    // Close sockets
+	    close(new_socket);
+	    close(server_fd);
+	    return 0;
+	}
 }
